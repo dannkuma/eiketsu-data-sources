9750cb34486c66dddbf4f9bef9870ecbe0dbff02
dannkuma <dannk0308@gmail.com>
2025-07-21 22:31:12 +0900

    変更点は以下 ・各コンテナの追加及び修正を実施 ・マルチプラットフォーム対応のDocker Imageを利用するよう修正 ・LaravelからMySQLへの接続を確認 ・Laravelからnginxへの接続を確認 ・Memcachedの動作確認をPintで実施 ・ngrokの動作確認を実施 ・spatie/Browsershotの動作確認を実施 ・Duskを利用したスクレイピングの動作確認を実施 ・Stripe Checkoutの動作確認を実施 ・Breezeのインストールを実施

---
+++ src/app/Http/Controllers/BrowsershotController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\BrowsershotService;
+
+class BrowsershotController extends Controller
+{
+    protected $BrowsershotService;
+    public function __construct(BrowsershotService $browsershotService)
+    {
+        $this->BrowsershotService = $browsershotService;
+    }
+
+    public function showDeckImage(Request $request)
+    {
+        // 武将画像を出力
+        $this->BrowsershotService->captureScreenshot(
+            'https://eiketsu-taisen.net/datalist/?v=general&s=general&c=b2febe07b7a680b2426067ee6331f611',
+            public_path('images/deck.png')
+        );
+
+        // 武将画像を表示
+        return response()->file(public_path('images/deck.png'));
+    }
+}
