ed52942b0cde81c150779550ed4fb437db29b832
dannkuma <dannk0308@gmail.com>
2025-12-16 18:28:49 +0900

    refactor: DatabaseSeederにSoulsSeederを追加し、GeneralsSeederのシード処理を修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\General\GeneralsSeeder;
 use Database\Seeders\Heirloom\HeirloomsSeeder;
+use Database\Seeders\Soul\SoulsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,6 +26,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             GeneralsSeeder::class,
             HeirloomsSeeder::class,
+            SoulsSeeder::class,
         ]);
     }
 }
