63c69aa300dc637db33189f40f9c1d937b047d56
dannkuma <dannk0308@gmail.com>
2026-01-12 10:44:31 +0900

    feat: フリガナカラムのnullable属性を変更するマイグレーションを追加 ・ 'generals' テーブルの 'name_furigana' カラムをnullableに変更 ・ 'strategy_name_furiganas' テーブルの 'strategy_name_furigana' カラムをnullableに変更 ・ 'heirlooms' テーブルの 'name_furigana' カラムをnullableに変更

---
+++ src/database/migrations/2026_01_12_103244_change_nullable_on_furigana_columns.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('generals', function (Blueprint $table) {
+            $table->string('name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable()->change();
+        });
+        Schema::table('strategy_name_furiganas', function (Blueprint $table) {
+            $table->string('strategy_name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable()->change();
+        });
+        Schema::table('heirlooms', function (Blueprint $table) {
+            $table->string('name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('generals', function (Blueprint $table) {
+            $table->string('name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable(false)->change();
+        });
+        Schema::table('strategy_name_furiganas', function (Blueprint $table) {
+            $table->string('strategy_name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable(false)->change();
+        });
+        Schema::table('heirlooms', function (Blueprint $table) {
+            $table->string('name_furigana')->collation('utf8mb4_ja_0900_as_cs')->nullable(false)->change();
+        });
+    }
+};
