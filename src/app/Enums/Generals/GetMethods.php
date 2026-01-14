190c147e4b2c21899cc1a8a935f7a8d3656e0f93
dannkuma <dannk0308@gmail.com>
2026-01-14 20:49:49 +0900

    feat: 武将用Seederの修正 ・入手手段のシーダーを本番データに修正 ・登場弾のシーダーを本番データに修正 ・声優のシーダーを本番データに修正 ・効果範囲のシーダーを本番データに修正 ・武将提供開始日のシーダーを本番データに修正 ・武将提供終了日のシーダーを本番データに修正 ・イラストレーターのシーダーを本番データに修正 ・計略名フリガナのシーダーを本番データに修正 ・計略名のシーダーを本番データに修正 ・計略説明のシーダーを本番データに修正 ・武将入手手段のシーダーを本番データに修正 ・武将のシーダーを本番データに修正 ・武将特技のシーダーを本番データに修正 ・武将計略カテゴリのシーダーを本番データに修正

--- src/app/Enums/Generals/GetMethods.php
+++ src/app/Enums/Generals/GetMethods.php
@@ -5,21 +5,37 @@ namespace App\Enums\Generals;
 enum GetMethods: string
 {
     case Starter = 'starter';         // スターター
-    case DiamondShop = 'diamond-shop';    // 金剛石商店(金剛石交換所)
     case SearchGeneral = 'search-general';  // 探索(武将)
-    case ObtainGeneral = 'obtain-general';  // 武将獲得
-    case StampsCampaign = 'stamps-campaign'; // 大戦スタンプキャンペーン
+    case AcquireGeneral = 'acquire-general';  // 武将獲得
+    case DiamondShop = 'diamond-shop';    // 金剛石商店(金剛石交換所)
     case BasaraShop = 'basara-shop';     // 金剛石商店(バサラ石交換所)
+    case PointShop = 'point-shop';      // ポイントショップイベント
+    case StampsCampaign = 'stamps-campaign'; // 大戦スタンプキャンペーン
+    case GoodsCampaignFirst = 'goods-campaign-first'; // グッズキャンペーン第1弾
+    case GoodsCampaignSecond = 'goods-campaign-second'; // グッズキャンペーン第2弾
+    case GoodsCampaignThird = 'goods-campaign-third'; // グッズキャンペーン第3弾
+    case GoodsCampaignFourth = 'goods-campaign-fourth'; // グッズキャンペーン第4弾
+    case PreviousGamePlayPrivilege = 'previous-game-play-privilege'; // 三国志大戦プレイ特典
+    case CommunityAndTwitterCampaign = 'community-and-twitter-campaign'; // 大戦組連携&Twitterフォローキャンペーン
+    case LaunchCommemorationCampaign = 'launch-commemoration-campaign'; // 英傑大戦稼働記念キャンペーン
 
     public function label(): string
     {
         return match ($this) {
             self::Starter => 'スターター',
-            self::DiamondShop => '金剛石商店(金剛石交換所)',
             self::SearchGeneral => '探索(武将)',
-            self::ObtainGeneral => '武将獲得',
-            self::StampsCampaign => '大戦スタンプキャンペーン',
+            self::AcquireGeneral => '武将獲得',
+            self::DiamondShop => '金剛石商店(金剛石交換所)',
             self::BasaraShop => '金剛石商店(バサラ石交換所)',
+            self::PointShop => 'ポイントショップイベント',
+            self::StampsCampaign => '大戦スタンプキャンペーン',
+            self::GoodsCampaignFirst => 'スペシャルグッズキャンペーン第1弾',
+            self::GoodsCampaignSecond => 'スペシャルグッズキャンペーン第2弾',
+            self::GoodsCampaignThird => 'スペシャルグッズキャンペーン第3弾',
+            self::GoodsCampaignFourth => 'スペシャルグッズキャンペーン第4弾',
+            self::PreviousGamePlayPrivilege => '三国志大戦プレイ特典',
+            self::CommunityAndTwitterCampaign => '大戦組連携&Twitterフォローキャンペーン',
+            self::LaunchCommemorationCampaign => '英傑大戦稼働記念キャンペーン',
         };
     }
 
