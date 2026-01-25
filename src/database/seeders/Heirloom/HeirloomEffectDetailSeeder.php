59fe9850c05191aeca841ef1c4aa1c08f43bcc33
dannkuma <dannk0308@gmail.com>
2026-01-25 19:56:43 +0900

    feat: HeirloomEffectDetailSeederのデータシードを本番データに更新

--- src/database/seeders/Heirloom/HeirloomEffectDetailSeeder.php
+++ src/database/seeders/Heirloom/HeirloomEffectDetailSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders\Heirloom;
 
-use App\Models\Heirloom\HeirloomEffect;
 use App\Models\Heirloom\HeirloomEffectDetail;
-use App\Models\Heirloom\HeirloomEnhancementType;
 use Illuminate\Database\Seeder;
 
 class HeirloomEffectDetailSeeder extends Seeder
@@ -14,13 +12,476 @@ class HeirloomEffectDetailSeeder extends Seeder
      */
     public function run(): void
     {
-        $heirloomEffect = HeirloomEffect::first();
-        $heirloomEnhancementType = HeirloomEnhancementType::first();
-        HeirloomEffectDetail::updateOrCreate(
+        $heirloomEffectDetails = [
             [
-                'heirloom_effect_id' => $heirloomEffect->id,
-                'heirloom_enhancement_type_id' => $heirloomEnhancementType->id,
+                'heirloom_effect_id' => 1,
+                'heirloom_enhancement_type_id' => null,
             ],
+            [
+                'heirloom_effect_id' => 2,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 3,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 4,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 5,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 6,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 7,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 8,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 9,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 10,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 11,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 12,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 13,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 14,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 15,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 15,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 16,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 17,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 17,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 18,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 19,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 20,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 21,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 22,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 23,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 24,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 25,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 26,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 27,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 28,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 29,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 30,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 31,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 18,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 16,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 32,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 33,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 33,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 34,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 35,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 35,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 36,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 36,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 37,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 37,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 38,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 39,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 39,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 40,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 40,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 41,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 41,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 42,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 42,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 43,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 43,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 44,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 44,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 45,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 45,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 24,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 46,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 46,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 38,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 47,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 47,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 48,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 48,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 49,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 50,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 51,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 52,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 52,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 51,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 53,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 53,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 54,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 54,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 55,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 55,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 56,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 57,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 58,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 58,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 59,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 59,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 60,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 57,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 61,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 62,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 62,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 63,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 63,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 64,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 64,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 65,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 65,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 66,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 66,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 67,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 67,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 68,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 68,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 69,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 70,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 71,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 72,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 73,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 74,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 75,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 76,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 77,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 78,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 78,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+            [
+                'heirloom_effect_id' => 79,
+                'heirloom_enhancement_type_id' => null,
+            ],
+            [
+                'heirloom_effect_id' => 79,
+                'heirloom_enhancement_type_id' => 1,
+            ],
+        ];
+
+        HeirloomEffectDetail::upsert(
+            $heirloomEffectDetails,
+            ['heirloom_effect_id', 'heirloom_enhancement_type_id'],
             []
         );
     }
